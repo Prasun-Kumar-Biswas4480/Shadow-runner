620467f8d980679fd99d5cfea05b0c367a7538bf
Prasun-Kumar-Biswas4480 <74824302+Prasun-Kumar-Biswas4480@users.noreply.github.com>
2021-08-04 21:53:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup() {
  
   ground = createSprite(width/2, displayHeight-160, width, 20);
   ground.velocityX = -3;
-  ground.shaprcolor = "black";
+  ground.shapecolor = "black";
 
   obstacleGroup = new Group();
 
